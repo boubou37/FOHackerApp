75075a6f08362c40ea09682eb27492db01402475
boubou37 <lrahamefy@sfr.fr>
2017-03-26 18:26:17 +0200

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { SolverComponent } from './solver/solver.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SolverComponent
   ],
   imports: [
     BrowserModule,
